26b64c53c650afaf2a1a29fb73d7f27387970a4b
nzi <nina.ms.zimmermann@web.de>
2025-02-14 00:52:00 +0100

    update: listen

--- buffer.py
+++ buffer.py
@@ -8,6 +8,18 @@ class Buffer:
     def send(self, message):
         self.socket.send(message.encode("utf-8"))
 
+    def listen(self):
+        try:
+            while True:
+                data = self.socket.recv(1024)
+                if not data:
+                    print("Server closed the connection.")
+                    break
+                print(f"Received from server: {data.decode('utf-8')}")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
 
 IP = '192.168.176.250'
 PORT = 12345
@@ -16,6 +28,8 @@ def runBuffer():
     buffer = Buffer(IP, PORT)
 
     buffer.send("test\n")
+
+    buffer.listen()
 
     buffer.socket.close()
 
