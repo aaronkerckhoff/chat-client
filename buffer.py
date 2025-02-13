a23640aa54e1787784175784be43b853ca733f5e
nzi <nina.ms.zimmermann@web.de>
2025-02-13 23:12:22 +0100

    buffer

--- buffer.py
+++ buffer.py
@@ -1,16 +1,20 @@
 import socket
 
-def send(ip, port, message):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((ip, port))
+class Buffer:
+    def __innit__(self):
+        self.ip
 
-    client_socket.send(message.encode())
+    def send(ip, port, message):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((ip, port))
 
-    client_socket.close()
+        client_socket.send(message.encode())
+
+        client_socket.close()
 
 
 IP = '192.168.176.250'
 PORT = 12345
 
 if __name__ == '__main__':
-    send(IP, PORT, 'test')
+    Buffer.send(IP, PORT, 'test')
