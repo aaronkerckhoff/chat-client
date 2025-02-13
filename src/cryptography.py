d8bad507f545eae44323844b0183dfce94e659b5
John Doe <johndoe@example.com>
2025-02-13 23:35:46 +0100

    add asym derypt func

--- src/cryptography.py
+++ src/cryptography.py
@@ -3,9 +3,28 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 
-def encrypt(pk, message):
-    pass
-def get_new_async_keys():
+def asym_encrypt(pk, message):
+    encrypted_message = pk.encrypt(
+        message,
+        padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+        )
+    )
+    return encrypted_message
+
+def asym_decryption(sk, message):
+    decrypted_message = sk.decrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+def get_new_asym_keys():
     private_key = ec.generate_private_key(ec.SECP256R1())
     public_key = private_key.public_key()
     return private_key, public_key
