b53d50b1b65deae35fc519c1e37ea518e603c803
aaronkerckhoff <aaronkerckhoff@gmail.com>
2025-02-14 00:46:06 +0100

    feat(cryptography): add aes key derivation function

--- src/crypto.py
+++ src/crypto.py
@@ -8,50 +8,62 @@ from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from dotenv import load_dotenv, set_key
 
+
+def generate_symmetric_key_128(shared_secret, key_length=32):
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(), length=key_length, salt=None, info=b"handshake data"
+    ).derive(shared_secret)
+
+    return derived_key
+
+
 # Load environment variables from .env file
 load_dotenv()
+
 
 def save_key_to_env(key, key_name):
     key_bytes = key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    ).decode('utf-8')
-    set_key('.env', key_name, key_bytes)
+        encryption_algorithm=serialization.NoEncryption(),
+    ).decode("utf-8")
+    set_key(".env", key_name, key_bytes)
+
 
 def save_public_key_to_env(key, key_name):
     key_bytes = key.public_bytes(
         encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    ).decode('utf-8')
-    set_key('.env', key_name, key_bytes)
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    ).decode("utf-8")
+    set_key(".env", key_name, key_bytes)
+
 
 def load_key_from_env(key_name):
     key_bytes = os.getenv(key_name)
     if key_bytes:
         return serialization.load_pem_private_key(
-            key_bytes.encode('utf-8'),
-            password=None
+            key_bytes.encode("utf-8"), password=None
         )
     return None
+
 
 def load_public_key_from_env(key_name):
     key_bytes = os.getenv(key_name)
     if key_bytes:
-        return serialization.load_pem_public_key(
-            key_bytes.encode('utf-8')
-        )
+        return serialization.load_pem_public_key(key_bytes.encode("utf-8"))
     return None
 
+
 def get_new_asym_keys():
-    secret_key = load_key_from_env('SECRET_KEY')
-    public_key = load_public_key_from_env('PUBLIC_KEY')
+    secret_key = load_key_from_env("SECRET_KEY")
+    public_key = load_public_key_from_env("PUBLIC_KEY")
     if secret_key is None or public_key is None:
         secret_key = ec.generate_private_key(ec.SECP256R1())
         public_key = secret_key.public_key()
-        save_key_to_env(secret_key, 'SECRET_KEY')
-        save_public_key_to_env(public_key, 'PUBLIC_KEY')
+        save_key_to_env(secret_key, "SECRET_KEY")
+        save_public_key_to_env(public_key, "PUBLIC_KEY")
     return secret_key, public_key
+
 
 def aes_encrypt(key, plaintext, associated_data=None):
     """
@@ -71,6 +83,7 @@ def aes_encrypt(key, plaintext, associated_data=None):
         "associated_data": associated_data,
     }
 
+
 def aes128_decrypt(key, encrypted_data):
     """
     Decrypt ciphertext using AES-128-GCM.
@@ -89,20 +102,19 @@ def aes128_decrypt(key, encrypted_data):
     return plaintext
 
 
-def eec_key_derivation(sk,peer_pk):
+def eec_key_derivation(sk, peer_pk):
     shared_secret = sk.exchange(ec.ECDH(), peer_pk)
     return shared_secret
 
+
 def get_asym_sig(sk, message):
-    message = message.encode('utf-8') 
-    signature = sk.sign(
-        message,
-        ec.ECDSA(hashes.SHA256())
-    )
+    message = message.encode("utf-8")
+    signature = sk.sign(message, ec.ECDSA(hashes.SHA256()))
     return signature
 
+
 def verify_asym_sig(pk, message, signature):
-    message = message.encode('utf-8') 
+    message = message.encode("utf-8")
     try:
         pk.verify(signature, message, ec.ECDSA(hashes.SHA256()))
         print("The signature is valid!")
@@ -111,6 +123,7 @@ def verify_asym_sig(pk, message, signature):
         print("The signature is invalid:", e)
         return False
 
+
 def key_derivation(pk):
     salt = os.urandom(16)
     key_length = 32  # Example key length
@@ -118,16 +131,18 @@ def key_derivation(pk):
         algorithm=hashes.SHA256(),
         length=2 * key_length,  # Derive twice the length of a single key
         salt=salt,
-        info=b'handshake data',
+        info=b"handshake data",
     )
     pk_bytes = pk.public_bytes(
         encoding=serialization.Encoding.DER,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        encryption_algorithm=serialization.NoEncryption(),
     )
     derived_key_material = hkdf.derive(pk_bytes)
     key1 = derived_key_material[:key_length]
     key2 = derived_key_material[key_length:]
     return key1, key2
+
 
 # Generate or load keys
 sk, pk = get_new_asym_keys()
