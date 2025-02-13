b1e29994c6d8ab58ca3dd15c6cf964c1e31b5334
nzi <nina.ms.zimmermann@web.de>
2025-02-14 00:33:42 +0100

    c

--- client.py
+++ client.py
@@ -1,16 +1,26 @@
 import socket
 
-def send(ip, port, message):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((ip, port))
 
-    client_socket.send(message.encode())
+class Client:
+    def __innit__(self):
+        ...
 
-    client_socket.close()
+    def send(ip, port, message):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((ip, port))
+
+        client_socket.send(message.encode())
+
+        client_socket.close()
 
 
 IP = '192.168.176.250'
 PORT = 12345
 
+def start():
+    client = Client()
+
+    client.send(IP, PORT, 'test')
+
 if __name__ == '__main__':
-    send(IP, PORT, 'test')
+    start()
