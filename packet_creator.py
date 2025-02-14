8622b65c72de78d28f5d3676029a3445aa01e16b
Leah Wolfschläger <e.wolfschlaeger@gmail.com>
2025-02-14 10:42:14 +0100

    added broadcast message creation

--- packet_creator.py
+++ packet_creator.py
@@ -12,14 +12,46 @@ def create_head() -> io.BytesIO:
     stream.write(client_specifics.to_bytes(2, byteorder="little"))
     return stream
 
+
+def as_bytes(body) -> bytes:
+    stream = create_head()
+    encoded_body = json.dumps(body).encode(encoding="utf-8")
+    stream.write(encoded_body)
+    return stream.read()
+
 def create_body(message_type):
     body = {
         "from_buf" : False,
         "type" : message_type
     }
     return body
+
+def create_broadcast_message():
+    body = create_body("BROADCAST")
+
+def create_exists_message(public_key: str, display_name: str, signature: str):
+    body = create_broadcast_message()
+    body["inner"] = {
+        "type": "EXISTS",
+        "public_key": public_key,
+        "display_name": display_name,
+        "sig": signature
+    }
+    return as_bytes(body)
+
+def create_wants_message(request_public_key: str):
+    body = create_broadcast_message()
+    body["inner"] = {
+        "type": "WANTS",
+        "public_key": request_public_key,
+    }
+    return as_bytes(body)
+
 def create_direct_message():
     body = create_body("DIRECTED")
     body["receiver"] = 
+
+
+
     
 
