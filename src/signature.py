99ae663c052a22396f7230a3c0ffa149955b5eb7
aaronkerckhoff <aaronkerckhoff@gmail.com>
2025-02-14 10:18:14 +0100

    feat(cryptography): implement signature class

--- src/signature.py
+++ src/signature.py
@@ -1,6 +1,13 @@
+from src.crypto import verify_asym_sig
+
+
 class Signature:
-    pass
+    def __init__(self, sig: str):
+        self.signature = sig
+
+    def verify(self, pk, message):
+        return verify_asym_sig(pk, message, self.signature)
 
 
 def from_base64_string(str: str) -> Signature:
-    pass
+    return Signature(str)
