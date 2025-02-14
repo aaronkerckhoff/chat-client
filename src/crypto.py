0c3dfe8554742d1e9fa630bb94580a5fb6aa8bf9
aaronkerckhoff <aaronkerckhoff@gmail.com>
2025-02-14 12:49:22 +0100

    feat(cryptography): add rsa encrypt function

--- src/crypto.py
+++ src/crypto.py
@@ -1,6 +1,6 @@
 import os
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 
 from src.logger_utils import setup_logger
 
@@ -11,6 +11,7 @@ PRIVATE_KEY_FILE = os.path.join(KEY_DIR, "private_key.pem")
 PUBLIC_KEY_FILE = os.path.join(KEY_DIR, "public_key.pem")
 
 
+# Key creation and loading helper functions
 def generate_rsa_key_pair():
     """
     Generates an RSA key pair and stores them as PEM files.
@@ -51,3 +52,19 @@ def load_public_key():
     with open(PUBLIC_KEY_FILE, "rb") as file:
         public_key = serialization.load_pem_public_key(file.read())
     return public_key
+
+
+# RSA functions
+def rsa_encrypt(public_key, data: bytes) -> bytes:
+    """
+    Encrypt data using recipient's RSA public key
+    """
+    ciphertext = public_key.encrypt(
+        data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None,
+        ),
+    )
+    return ciphertext
